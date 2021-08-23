ef79c767dc809fbf542b6d28c0cb2b6cc1046d56
vanessa <wjssazpjya@gmail.com>
2021-08-23 19:50:25 +0800

    20210823

--- common/spring_security/src/main/java/icu/junyao/security/security/UnauthorizedEntryPoint.java
+++ common/spring_security/src/main/java/icu/junyao/security/security/UnauthorizedEntryPoint.java
@@ -22,7 +22,7 @@ public class UnauthorizedEntryPoint implements AuthenticationEntryPoint {
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
-                         AuthenticationException authException) throws IOException, ServletException {
+                         AuthenticationException authException) {
 
         ResponseUtil.out(response, R.error());
     }
